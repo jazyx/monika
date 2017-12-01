44f5d3f34de7c6cb62f1ede896cacc6025a81c58
James Newton <github@lexogram.com>
2017-12-01 22:30:06 +0300

    Add JavaScript for changing levels
    - Using jQuery to hot-swap the articles element
    - Add layouts/<levelName>/level.html and level.css files
    - Add js/<levelName>.js files
    - Using "splash" as the empty placeholder for the start-up page
    - Levels 1 and 2 set the buttons to red and green)

---
+++ js/intro.js
@@ -0,0 +1,30 @@
+;(function levelLoaded(level){
+  // level = { map: { <name>: <object>, ... }, name: <string> }
+  
+  function Level() {
+    this.name = "Intro"
+
+    console.log(this.name + " is instantiated")
+  }
+
+  Level.prototype.initialize = function initialize() {
+    console.log(this.name + " is being initialized")
+
+    //div.addEventListener("transitionend", levelComplete, false)
+
+
+    function levelComplete(event) {
+      level.completed(level.name)
+    }
+  }
+
+  Level.prototype.kill = function kill() {
+    // Clean up when level is about to be replaced
+  }
+
+  if (typeof level.name === "string") {
+    if (typeof level.map === "object") {
+      var object = level.map[level.name] = new Level()
+    }
+  }
+})(window.level) // <HARD-CODED global object>
---
+++ js/splash.js
@@ -0,0 +1,3 @@
+;(function splash(){
+  console.log("Splash here. Just lettin' you know.")
+})()
